86b671c6fccc36a09cdfba7ec3db800f065b30a4
sumankumar <sumankumar@fico.com>
2024-06-01 15:36:09 +0530

    Added code for find next greater ele to left

--- stack/NextGreatNumberToLeft.java
+++ stack/NextGreatNumberToLeft.java
@@ -1,7 +1,32 @@
 package DataStructure.stack;
 
+import java.util.Arrays;
+import java.util.Stack;
+import java.util.stream.Stream;
+
+/*
+find the next greater elements to its left
+ */
 public class NextGreatNumberToLeft {
     public static void main(String[] args) {
-        System.out.println("SUman Kumar:");
+        int[] arr={4,3,6,8,9,1,11};
+        findNextGreaterToLeft(arr);
+    }
+    private static void findNextGreaterToLeft(int[] arr) {
+      Stack<Integer> stack = new Stack<>();
+      String[] result = new String[arr.length];
+        for(int i=0;i<arr.length;i++){
+            while(!stack.isEmpty() && stack.peek()<arr[i]){
+                stack.pop();
+            }
+            if(stack.isEmpty()){
+                result[i]="_";
+            }
+            else {
+                result[i]=String.valueOf(stack.peek());
+            }
+            stack.push(arr[i]);
+        }
+        Arrays.stream(result).forEach(e->System.out.print(e+" "));
     }
 }
