615f57766b7f55ed5e2758f16cd262c4359bdc35
sumankumar <sumankumar@fico.com>
2024-06-02 14:44:55 +0530

    Anagram concepts

---
+++ Java8/StringAngram.java
@@ -0,0 +1,19 @@
+package DataStructure.Java8;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StringAngram {
+    public static void main(String[] args) {
+        String input = "abced";
+        String s = "dceab";
+        Boolean isAnagram = checkAnagram(input, s);
+        System.out.println(isAnagram ? "Anagram" : "Not Anagram");
+    }
+
+    private static Boolean checkAnagram(String input, String input1) {
+        List<String> list = new ArrayList<>();
+        return input.chars().mapToObj(e -> (char) e).sorted().toList().equals(input1.chars().mapToObj(e -> (char) e).sorted().toList());
+    }
+
+}
