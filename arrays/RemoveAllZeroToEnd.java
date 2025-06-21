4beea77af71da5ac21e5b812aa9e7a3a206a9e33
Suman <suman@Sumans-MacBook-Pro.local>
2025-06-21 11:15:01 +0530

    move all zero to end

---
+++ arrays/RemoveAllZeroToEnd.java
@@ -0,0 +1,35 @@
+package DataStructure.arrays;
+
+import java.util.Arrays;
+
+public class RemoveAllZeroToEnd {
+    public static void main( String[] args ) {
+        int rawArray[] = { 1, 3, 0, 4, 0, 7, 9 };
+        shiftAllZeroToRight( rawArray );
+        printArray( rawArray );
+    }
+
+    private static void printArray( int[] rawArray ) {
+        Arrays.stream( rawArray ).forEach( System.out::print );
+    }
+
+    private static void shiftAllZeroToRight( int[] rawArray ) {
+        int n = rawArray.length;
+        int j = 0;
+        for ( ; j < n - 1; j++ ) {
+            if ( rawArray[j] == 0 ) {
+                break;
+            }
+        }
+        for ( int i = j + 1; i < n; i++ ) {
+            if ( rawArray[i] != 0 ) {
+                rawArray[j] = rawArray[i];
+                j++;
+            }
+        }
+        while ( j < n ) {
+            rawArray[j] = 0;
+            j++;
+        }
+    }
+}
