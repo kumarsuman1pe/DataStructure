25bf8fa0fcad6ff9b7d18e85e56845c583cecb3e
Suman <suman@Sumans-MacBook-Pro.local>
2025-06-21 11:17:53 +0530

    move all zero to end

--- arrays/ShiftAllZeroToEnd.java
+++ arrays/ShiftAllZeroToEnd.java
@@ -2,7 +2,7 @@ package DataStructure.arrays;
 
 import java.util.Arrays;
 
-public class RemoveAllZeroToEnd {
+public class ShiftAllZeroToEnd {
     public static void main( String[] args ) {
         int rawArray[] = { 1, 3, 0, 4, 0, 7, 9 };
         shiftAllZeroToRight( rawArray );
